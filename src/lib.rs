f4650d8908553f715f364f4ce400b8f2bbddf6af
Zageron <hello@adambryant.ca>
2021-04-19 21:56:13 -0700

    Moving along with rendering pare down for simplicity.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-use std::io::BufRead;
-
+use render::State;
 use wasm_bindgen::prelude::*;
-use winit::event_loop::EventLoop;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+mod render;
 
 pub struct Store {
     pub local_storage: web_sys::Storage,
@@ -18,7 +23,7 @@ fn save_to_localstore(name: &str, item: &str) {
 fn load_from_localstore(item: &str) -> Option<String> {
     let window = web_sys::window();
     if let Ok(Some(local_storage)) = window.unwrap().local_storage() {
-        let mut store = Store { local_storage };
+        let store = Store { local_storage };
         if let Ok(Some(value)) = store.local_storage.get_item(item) {
             Some(value)
         } else {
@@ -43,23 +48,58 @@ fn load_obj(object: &str, material: &str) {
     log::info!("object: {:?}", m);
 }
 
-async fn run() {
-    #[cfg_attr(target_arch = "wasm32", allow(unused_variables))]
-    let adapter = wgpu::Instance::new(wgpu::BackendBit::PRIMARY)
-        .request_adapter(&wgpu::RequestAdapterOptions::default())
-        .await
-        .unwrap();
+async fn run(event_loop: EventLoop<()>, window: Window) {
+    let mut state = State::new(&window).await;
 
-    #[cfg_attr(target_arch = "wasm32", allow(unused_variables))]
-    log::info!("Adapter Information: {:?}", adapter);
+    let size = window.inner_size();
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                state.resize(size);
+            }
+            Event::RedrawRequested(_) => {
+                state.render();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
 
-    let event_loop = EventLoop::new();
-    let mut builder = winit::window::WindowBuilder::new();
-    builder = builder.with_title("Window");
-    let window = builder.build(&event_loop).unwrap();
+            Event::NewEvents(_) => {}
+            Event::DeviceEvent { device_id, event } => {}
+            Event::UserEvent(_) => {}
+            Event::Suspended => {
+                log::info!("Suspended");
+            }
+            Event::Resumed => {
+                log::info!("Resumed");
+            }
+            Event::MainEventsCleared => {}
+            Event::RedrawEventsCleared => {}
+            Event::LoopDestroyed => {}
+            _ => {}
+        }
+    });
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
     #[cfg_attr(target_arch = "wasm32", allow(unused_variables))]
     {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init().expect("could not initialize logger");
+
+        let event_loop = EventLoop::new();
+        let mut builder = winit::window::WindowBuilder::new();
+        builder = builder.with_title("Window");
+        let window = builder.build(&event_loop).unwrap();
+
         use winit::platform::web::WindowExtWebSys;
+
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
         // On wasm, append the canvas to the document body
         web_sys::window()
@@ -70,52 +110,9 @@ async fn run() {
                     .ok()
             })
             .expect("couldn't append canvas to document body");
-    }
 
-    // let paddle_asset = b"# Blender v2.91.2 OBJ File: 'Paddle.blend'
-    // # www.blender.org
-    // mtllib Paddle.mtl
-    // o Plane
-    // v -1.000000 0.000000 0.000000
-    // v 1.000000 0.000000 0.000000
-    // v -1.000000 0.000000 -1.000000
-    // v 1.000000 0.000000 -1.000000
-    // vt 0.000000 0.000000
-    // vt 1.000000 0.000000
-    // vt 1.000000 1.000000
-    // vt 0.000000 1.000000
-    // vn 0.0000 1.0000 0.0000
-    // usemtl None
-    // s off
-    // f 1/1/1 2/2/1 4/3/1 3/4/1
-    // ";
-
-    // let paddle_mtl = b"# Blender MTL File: 'Paddle.blend'
-    // # Material Count: 1
-
-    // newmtl None
-    // Ns 500
-    // Ka 0.8 0.8 0.8
-    // Kd 0.8 0.8 0.8
-    // Ks 0.8 0.8 0.8
-    // d 1
-    // illum 2
-    // ";
-
-    // save_to_localstore("paddle_asset", base64::encode(paddle_asset).as_str());
-    // save_to_localstore("paddle_mtl", base64::encode(paddle_mtl).as_str());
-
-    let paddle_asset = load_from_localstore("paddle_asset");
-    let paddle_mtl = load_from_localstore("paddle_mtl");
-    load_obj(paddle_asset.unwrap().as_str(), paddle_mtl.unwrap().as_str());
-}
-
-#[wasm_bindgen(start)]
-pub fn main() {
-    #[cfg_attr(target_arch = "wasm32", allow(unused_variables))]
-    {
-        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        console_log::init().expect("could not initialize logger");
-        wasm_bindgen_futures::spawn_local(run());
+        let event_loop = EventLoop::new();
+        let window = winit::window::Window::new(&event_loop).unwrap();
+        wasm_bindgen_futures::spawn_local(run(event_loop, window));
     }
 }
---
+++ src/render/mod.rs
@@ -0,0 +1,133 @@
+use std::iter;
+use winit::window::Window;
+
+pub struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+    clear_color: wgpu::Color,
+}
+
+impl State {
+    pub async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        let texture_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler {
+                            comparison: false,
+                            filtering: true,
+                        },
+                        count: None,
+                    },
+                ],
+                label: Some("texture_bind_group_layout"),
+            });
+
+        // TODO: Move these shaders out of statically linking and move them elsewhere.
+        let vs_module =
+            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.vert.spv"));
+        let fs_module =
+            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
+
+        let clear_color = wgpu::Color::BLACK;
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+            clear_color,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    pub fn update(&mut self) {}
+
+    pub fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        let frame = self.swap_chain.get_current_frame()?.output;
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+
+        Ok(())
+    }
+}
